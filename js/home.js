4ccc0f131f2cbb743937bc479a7154a2ce980f9d
latp1102 <latp1102@gamil.com>
2025-04-09 09:24:43 +0700

    first commit

--- js/home.js
+++ js/home.js
@@ -18,3 +18,17 @@ searchInput.addEventListener("input", function () {
     }
   });
 });
+// Đăng xuất
+const userData = JSON.parse(localStorage.getItem('userData'));
+
+if (userData) {
+    document.getElementById('userName').textContent = userData.email; 
+} else {
+    document.getElementById('logoutBtn').style.display = 'none';
+}
+
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    localStorage.removeItem('userData');
+
+    window.location.href = 'login.html';
+});
